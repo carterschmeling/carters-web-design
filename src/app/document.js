4844f5b3eeed6b703ec6367c76ec077febe5d4e6
carterschmeling <61914150+carterschmeling@users.noreply.github.com>
2026-01-24 21:38:15 -0600

    Updated document.js with correct Open Graph meta tags for carterschmeling.com

--- src/app/document.js
+++ src/app/document.js
@@ -16,12 +16,12 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
 
           {/* Add Open Graph meta tags */}
-          <meta property="og:image" content="https://carterscheling.com/logo.png" />
+          <meta property="og:image" content="https://carterschmeling.com/logo.png" />
           <meta property="og:image:width" content="1200" />
           <meta property="og:image:height" content="630" />
           <meta property="og:title" content="Carter's Web Design" />
           <meta property="og:description" content="Premium websites for small businesses." />
-          <meta property="og:url" content="carterscheling.com" />
+          <meta property="og:url" content="https://carterschmeling.com" />
           <meta property="og:type" content="website" />
         </Head>
         <body>
